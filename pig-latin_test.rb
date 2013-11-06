5ecd9e887f8914f96708ae845ee2244c3b2a502b
Michael Pavling <pavling@gmail.com>
2013-11-06 11:24:32 +0000

    passing next few tests with existing code

--- pig-latin_test.rb
+++ pig-latin_test.rb
@@ -8,17 +8,14 @@ class PigLatinTest < MiniTest::Unit::TestCase
   end
 
   def test_other_word_beginning_e
-    skip
     assert_equal "earay", PigLatin.translate("ear")
   end
 
   def test_word_beginning_with_p
-    skip
     assert_equal "igpay", PigLatin.translate("pig")
   end
 
   def test_word_beginning_with_k
-    skip
     assert_equal "oalakay", PigLatin.translate("koala")
   end
 
